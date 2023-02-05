5a8131609bc41c62634fe68791fa351cd3bbe499
Oli Hewison <hewisonoliver@gmail.com>
2023-02-05 15:32:32 +0000

    Added scenes to SceneManager

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -6,11 +6,15 @@ using UnityEngine;
 public class Collectible : MonoBehaviour
 {
     private bool _collected = false;
+    public AudioSource audioSource;
     private void Collect()
     {
         if (_collected) return;
         StageManager.Instance.Collect(this);
         _collected = true;
+        audioSource.pitch = Mathf.Lerp(1.0F, 1.5F,
+            (StageManager.Instance.collectibles % 8) / 8.0F);
+        audioSource.Play();
         gameObject.SetActive(false);
     }
 
